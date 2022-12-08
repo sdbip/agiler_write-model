af79b30a099119bcff7bbd7ba6da8efd3545f322
Johan Segerfeldt <johan.segerfeldt@me.com>
2022-12-08 06:45:42 +0100

    Add CanonicalEntityId

---
+++ src/es/canonical-entity-id.ts
@@ -0,0 +1,12 @@
+import { failFast } from './fail-fast.js'
+
+export class CanonicalEntityId {
+  constructor(readonly id: string, readonly type: string) {
+    failFast.unlessString(id, 'id')
+    failFast.unlessString(type, 'type')
+  }
+
+  equals(other: CanonicalEntityId): any {
+    return other.id === this.id && other.type === this.type
+  }
+}
---
+++ test/es/canonical-entity-id.spec.ts
@@ -0,0 +1,17 @@
+import { assert } from 'chai'
+import { CanonicalEntityId } from '../../src/es/canonical-entity-id.js'
+
+describe(CanonicalEntityId.name, () => {
+
+  it('equals if same id and type', () => {
+    assert.isTrue(new CanonicalEntityId('id', 'type').equals(new CanonicalEntityId('id', 'type')))
+  })
+
+  it('does not equal if id is different', () => {
+    assert.isFalse(new CanonicalEntityId('id', 'type').equals(new CanonicalEntityId('other_id', 'type')))
+  })
+
+  it('does not equal if type is different', () => {
+    assert.isFalse(new CanonicalEntityId('id', 'type').equals(new CanonicalEntityId('id', 'other_type')))
+  })
+})
